912f98f008bdb3f43cf2ade9220ed8b57e2395bf
thomas <tdegave@gmail.com>
2021-01-06 08:00:44 +0000

    Ajout Entity Mariage a l'api

--- src/Entity/Mariage.php
+++ src/Entity/Mariage.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Mariage
  *
+ * @ApiResource
  * @ORM\Table(name="mariage")
  * @ORM\Entity
  */
